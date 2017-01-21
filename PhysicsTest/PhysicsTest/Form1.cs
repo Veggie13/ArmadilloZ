860dae5dcad9acb96cd21bb8409f523bc404562e
Veggie13 <cmderochie+github@gmail.com>
2017-01-21 14:07:18 -0700

    Moving gitignores, added PhysicsTest

---
+++ PhysicsTest/PhysicsTest/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhysicsTest
+{
+    public partial class Form1 : Form
+    {
+        Scene scene = new Scene()
+        {
+            Objects = new List<SceneObject>()
+            {
+                new SceneObject()
+                {
+                    Position = new Point2() { X = 100f, Y = 100f },
+                    DragCoefficient = 0.5f,
+                    StopSpeed = 5f
+                }
+            }
+        };
+        Timer timer = new Timer() { Interval = 200 };
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            scene.Tick(0.2f);
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            g.FillRectangle(Brushes.Black, this.ClientRectangle);
+            foreach (var region in scene.Regions)
+            {
+                var circle = region.Region as Circle2;
+                if (circle != null)
+                {
+                    g.DrawEllipse(Pens.Red, circle.Center.X - circle.Radius, circle.Center.Y - circle.Radius, 2f * circle.Radius, 2f * circle.Radius);
+                }
+                var annulus = region.Region as Annulus2;
+                if (annulus != null)
+                {
+                    g.DrawEllipse(Pens.Red, annulus.Center.X - annulus.InnerRadius, annulus.Center.Y - annulus.InnerRadius, 2f * annulus.InnerRadius, 2f * annulus.InnerRadius);
+                    g.DrawEllipse(Pens.Red, annulus.Center.X - annulus.OuterRadius, annulus.Center.Y - annulus.OuterRadius, 2f * annulus.OuterRadius, 2f * annulus.OuterRadius);
+                    g.DrawLine(Pens.Red, annulus.Center.X, annulus.Center.Y, annulus.Center.X + annulus.OuterRadius * (float)Math.Cos(annulus.MinArc), annulus.Center.Y + annulus.OuterRadius * (float)Math.Sin(annulus.MinArc));
+                    g.DrawLine(Pens.Red, annulus.Center.X, annulus.Center.Y, annulus.Center.X + annulus.OuterRadius * (float)Math.Cos(annulus.MaxArc), annulus.Center.Y + annulus.OuterRadius * (float)Math.Sin(annulus.MaxArc));
+                }
+                var poly = region.Region as Polygon2;
+                if (poly != null)
+                {
+                    g.DrawPolygon(Pens.Red, poly.Points.Select(p => new PointF(p.X, p.Y)).ToArray());
+                }
+            }
+            foreach (var obj in scene.Objects)
+            {
+                g.FillRectangle(Brushes.Blue, obj.Position.X - 1f, obj.Position.Y - 1f, 3f, 3f);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Enabled = true;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            var newRegion = new RadialWaveForceRegion()
+            {
+                Center = new Point2() { X = e.X, Y = e.Y },
+                AnnularRegion = new Annulus2()
+                {
+                    Center = new Point2() { X = e.X, Y = e.Y },
+                    InnerRadius = 0f,
+                    OuterRadius = 40f,
+                    MinArc = 0f,
+                    MaxArc = (float)(Math.PI / 2)
+                },
+                Speed = 10f,
+                UnitAmplitude = 1f
+            };
+            scene.Regions.Add(newRegion);
+        }
+    }
+}
---
+++ PhysicsTest/PhysicsTest/Point3.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysicsTest
+{
+    struct Point2
+    {
+        public float X, Y;
+
+        public static Vector2 operator -(Point2 a, Point2 b)
+        {
+            return new Vector2() { X = a.X - b.X, Y = a.Y - b.Y };
+        }
+
+        public static Point2 operator +(Point2 a, Vector2 b)
+        {
+            return new Point2() { X = a.X + b.X, Y = a.Y + b.Y };
+        }
+    }
+
+    struct Vector2
+    {
+        public float X, Y;
+
+        public Vector2 Unit
+        {
+            get
+            {
+                float mag = Magnitude;
+                if (mag == 0f) return new Vector2();
+                return this * (1f / mag);
+            }
+        }
+        public float Magnitude { get { return (float)Math.Sqrt(this * this); } }
+
+        public static Vector2 operator -(Vector2 a, Vector2 b)
+        {
+            return new Vector2() { X = a.X - b.X, Y = a.Y - b.Y };
+        }
+
+        public static Vector2 operator +(Vector2 a, Vector2 b)
+        {
+            return new Vector2() { X = a.X + b.X, Y = a.Y + b.Y };
+        }
+
+        public static Vector2 operator *(Vector2 a, float b)
+        {
+            return new Vector2() { X = a.X * b, Y = a.Y * b };
+        }
+
+        public static float operator *(Vector2 a, Vector2 b)
+        {
+            return a.X * b.X + a.Y * b.Y;
+        }
+    }
+
+    abstract class Region2
+    {
+        public abstract bool Contains(Point2 point);
+    }
+
+    class Polygon2 : Region2
+    {
+        public List<Point2> Points = new List<Point2>();
+
+        public override bool Contains(Point2 point)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Circle2 : Region2
+    {
+        public Point2 Center;
+        public float Radius;
+
+        public override bool Contains(Point2 point)
+        {
+            return (point - Center).Magnitude < Radius;
+        }
+    }
+
+    class Annulus2 : Region2
+    {
+        public Point2 Center;
+        public float InnerRadius;
+        public float OuterRadius;
+        public float MinArc;
+        public float MaxArc;
+
+        public override bool Contains(Point2 point)
+        {
+            var radial = point - Center;
+            float radius = radial.Magnitude;
+            if (!(InnerRadius < radius && radius < OuterRadius)) return false;
+            float bearing = (float)Math.Atan2(radial.Y, radial.X);
+            double normBearing = (bearing - MinArc + Math.PI * 2) % (Math.PI * 2);
+            double arc = (MaxArc - MinArc + Math.PI * 2) % (Math.PI * 2);
+            return (normBearing < arc);
+        }
+    }
+
+    abstract class WaveForceRegion
+    {
+        public float Wavelength;
+
+        public abstract Region2 Region { get; }
+
+        public abstract float GetAmplitude(Point2 pos);
+
+        public Vector2 GetVelocity(Point2 pos)
+        {
+            if (!Region.Contains(pos))
+            {
+                return new Vector2();
+            }
+
+            return getVelocity(pos);
+        }
+
+        public Vector2 GetForce(Point2 pos, Vector2 velocity)
+        {
+            if (!Region.Contains(pos))
+            {
+                return new Vector2();
+            }
+
+            var waveVelocity = getVelocity(pos);
+            float relativeSpeed = ((velocity - waveVelocity) * waveVelocity) * -1f;
+            if (relativeSpeed < 0f)
+            {
+                return new Vector2();
+            }
+
+            return waveVelocity.Unit * relativeSpeed * GetAmplitude(pos);
+        }
+
+        protected abstract Vector2 getVelocity(Point2 pos);
+
+        public abstract float GetWaveValue(Point2 pos);
+
+        public abstract void Tick(float dt);
+    }
+
+    //class LinearWaveForceRegion : WaveForceRegion
+    //{
+    //    public float Amplitude;
+    //    public Vector2 Velocity;
+
+    //    public override float GetAmplitude(Point2 pos)
+    //    {
+    //        return Amplitude;
+    //    }
+
+    //    protected override Vector2 getVelocity(Point2 pos)
+    //    {
+    //        return Velocity;
+    //    }
+    //}
+
+    class RadialWaveForceRegion : WaveForceRegion
+    {
+        public Annulus2 AnnularRegion;
+        public Point2 Center;
+        public float Speed;
+        public float UnitAmplitude;
+
+        public override Region2 Region
+        {
+            get
+            {
+                return AnnularRegion;
+            }
+        }
+
+        protected override Vector2 getVelocity(Point2 pos)
+        {
+            var radial = pos - Center;
+            return radial.Unit * Speed;
+        }
+
+        public override float GetAmplitude(Point2 pos)
+        {
+            var radial = pos - Center;
+            float interp = (radial.Magnitude - AnnularRegion.InnerRadius) / (AnnularRegion.OuterRadius - AnnularRegion.InnerRadius);
+            return UnitAmplitude * interp;
+        }
+
+        public override void Tick(float dt)
+        {
+            AnnularRegion.OuterRadius += Speed * dt;
+            AnnularRegion.InnerRadius += Speed * dt;
+        }
+
+        public override float GetWaveValue(Point2 pos)
+        {
+            var radial = pos - Center;
+            float interp = 1f - (radial.Magnitude - AnnularRegion.InnerRadius) / (AnnularRegion.OuterRadius - AnnularRegion.InnerRadius);
+            float phase = interp / Wavelength;
+            return GetAmplitude(pos) * (float)Math.Sin(phase * Math.PI * 2);
+        }
+    }
+
+    class SceneObject
+    {
+        public Point2 Position;
+        public Vector2 Velocity;
+        public float DragCoefficient;
+        public float StopSpeed;
+
+        public void AcceptForce(Vector2 force, float dt)
+        {
+            Velocity += force * dt;
+        }
+
+        public void Tick(float dt)
+        {
+            Position += Velocity * dt;
+            Velocity *= (float)Math.Pow(DragCoefficient, dt);
+            if (Velocity.Magnitude < StopSpeed)
+            {
+                Velocity = new Vector2();
+            }
+        }
+    }
+
+    class Scene
+    {
+        public List<SceneObject> Objects = new List<SceneObject>();
+        public List<WaveForceRegion> Regions = new List<WaveForceRegion>();
+
+        public void Tick(float dt)
+        {
+            foreach (var obj in Objects)
+            {
+                foreach (var region in Regions)
+                {
+                    region.Tick(dt);
+                    obj.AcceptForce(region.GetForce(obj.Position, obj.Velocity), dt);
+                }
+
+                obj.Tick(dt);
+            }
+        }
+    }
+}
