105d9947ffb99ea4e3a2d029a4ab343f81bf5401
Adam Wall <agcwall@gmail.com>
2017-01-22 10:10:57 -0700

    add menus as seperate scene

---
+++ ArmadilloZ/Assets/Scripts/Menus.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menus : MonoBehaviour {
+    public Canvas startMenu;
+    public Canvas quitMenu;
+
+    public Button resume;
+
+	void Start () {
+        resume.enabled = false;
+        ShowMenu();
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("Cancel")) {
+            ShowMenu();
+        }
+    }
+
+    public void OnePlayer() {
+        resume.enabled = true;
+        Resume();
+    }
+
+    public void TwoPlayer() {
+        resume.enabled = true;
+        Resume();
+    }
+
+    public void Resume() {        
+        quitMenu.enabled = false;
+        startMenu.enabled = false;
+    }
+
+    public void Quit() {
+        quitMenu.enabled = true;
+        startMenu.enabled = false;
+    }
+
+    public void QuitNo() {
+        quitMenu.enabled = false;
+        startMenu.enabled = true;        
+    }
+
+    public void QuitYes() {
+        EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
+    public void ShowMenu() {        
+        quitMenu.enabled = false;
+        startMenu.enabled = true;
+    }
+}
