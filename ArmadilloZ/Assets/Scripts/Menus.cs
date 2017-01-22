4a7a3333efe2e8b7c423bf8adb65b15eeea2fde3
HARRISON\Peter <thepeterharrison@gmail.com>
2017-01-22 15:16:42 -0700

    Menu Changes

--- ArmadilloZ/Assets/Scripts/Menus.cs
+++ ArmadilloZ/Assets/Scripts/Menus.cs
@@ -7,26 +7,49 @@ using UnityEngine.UI;
 public class Menus : MonoBehaviour {
     public Canvas startMenu;
     public Canvas quitMenu;
-
+	public Image background;
     public Button resume;
+	public float maxScrollDistance;
+
+	public Vector2 backgroundScrollSpeed;
+	public Vector2 backgroundScrollDirection;
+	public Image foreground1;
+	public Image foreground2;
+	private float lastswitch;
+	private Vector3 originalPosition;
 
 	void Start () {
         resume.enabled = false;
-        ShowMenu();
+		foreground2.enabled = false;
+		originalPosition = background.transform.position;
+		ShowMenu();
     }
 
-    void Update() {
+    void Update() {		
+		float distance = System.Math.Abs (background.transform.position.x - originalPosition.x);
+		if (distance < maxScrollDistance) {
+			var movement = new Vector3(
+				backgroundScrollSpeed.x * backgroundScrollDirection.x,
+				backgroundScrollSpeed.y * backgroundScrollDirection.y,
+				0);
+			
+			movement *= Time.deltaTime;
+			background.transform.Translate (movement);
+		}
+		lastswitch += Time.deltaTime;
+
+		if (lastswitch > 1.0f) {
+			foreground1.enabled = !foreground1.enabled;
+			foreground2.enabled = !foreground1.enabled;
+			lastswitch = 0f;
+		}
+
         if (Input.GetButtonDown("Cancel")) {
             ShowMenu();
         }
     }
 
     public void OnePlayer() {
-        resume.enabled = true;
-        Resume();
-    }
-
-    public void TwoPlayer() {
         resume.enabled = true;
         Resume();
     }
@@ -54,5 +77,6 @@ public class Menus : MonoBehaviour {
     public void ShowMenu() {        
         quitMenu.enabled = false;
         startMenu.enabled = true;
+		background.transform.position = originalPosition;
     }
 }
