c2c3897cdc0f5507e4964226a1c1e095a1bf316e
HARRISON\Peter <thepeterharrison@gmail.com>
2017-01-21 20:56:45 -0700

    Committing all files - especially new testing project working with sand and water

--- New Unity Project/Assets/Scripts/CreateRadialPlane.cs
+++ New Unity Project/Assets/Scripts/CreateRadialPlane.cs
@@ -6,14 +6,13 @@ public class CreateRadialPlane : MonoBehaviour {
 
 	MeshFilter mf;
 
-
 	// Use this for initialization
 	void Start () {
 
 		Mesh mesh = new Mesh ();
 		mf = GetComponent<MeshFilter> ();
 		mf.mesh = mesh;
-		int radius = 10;
+		int radius = 100;
 		float rad60 = 1.047197f;
 	    float edgesize = 0.25f;
 		float triheight = edgesize * Mathf.Sin (rad60);
@@ -40,8 +39,8 @@ public class CreateRadialPlane : MonoBehaviour {
 				float x = (col + Mathf.Abs (radius - row) / 2f - radius) * edgesize;
 				float z = (radius - row) * triheight;
 				float s = Mathf.Sqrt (x * x + z * z);
-				float y = amplitude * Mathf.Cos (kmultiplier * s);
-				vertices [i] = new Vector3 (x, y, z);
+				//float y = amplitude * Mathf.Cos (kmultiplier * s);
+				vertices [i] = new Vector3 (x, 0, z);
 				i++;
 
 			}
@@ -101,45 +100,45 @@ public class CreateRadialPlane : MonoBehaviour {
 
 	void Update () {
 
-		Mesh mesh = mf.mesh;
-		int radius = 10;
-		float rad60 = 1.047197f;
-		float edgesize = 0.25f;
-		float triheight = edgesize * Mathf.Sin (rad60);
-		int numvert = 0;
-		int i;
-		float amplitude = 0.2f;
-		float kmultiplier = 5f;
-
-		for (i = 0; i <= radius; i++)
-			numvert = numvert + i;
-
-		numvert = numvert * 6 + 1;
-		int numtrianglepoints = radius * radius * 6 * 3; //Multiply by 3 since each triangle consists of 3 vertices
-
-		Vector3[] vertices = new Vector3[numvert];
-		int[] triangles = new int[numtrianglepoints];
-
-		i = 0;
-
-		for (int row = 0; row < (radius * 2 + 1); row++) 
-		{
-			for (int col = 0; col < ((radius * 2 + 1) - Mathf.Abs (radius - row)); col++) 
-			{
-				float x = (col + Mathf.Abs (radius - row) / 2f - radius) * edgesize;
-				float z = (radius - row) * triheight;
-				float s = Mathf.Sqrt (x * x + z * z);
-				float y = amplitude * Mathf.Cos (kmultiplier * s - (Time.fixedTime));
-				vertices [i] = new Vector3 (x, y, z);
-				i++;
-
-			}
-		}
-
-		mesh.vertices = vertices;
-		mesh.RecalculateNormals();
-		GetComponent<MeshCollider> ().sharedMesh = null;
-		GetComponent<MeshCollider> ().sharedMesh = mesh;
+//		Mesh mesh = mf.mesh;
+//		int radius = 100;
+//		float rad60 = 1.047197f;
+//		float edgesize = 0.25f;
+//		float triheight = edgesize * Mathf.Sin (rad60);
+//		int numvert = 0;
+//		int i;
+//		float amplitude = 0.2f;
+//		float kmultiplier = 5f;
+//
+//		for (i = 0; i <= radius; i++)
+//			numvert = numvert + i;
+//
+//		numvert = numvert * 6 + 1;
+//		int numtrianglepoints = radius * radius * 6 * 3; //Multiply by 3 since each triangle consists of 3 vertices
+//
+//		Vector3[] vertices = new Vector3[numvert];
+//		int[] triangles = new int[numtrianglepoints];
+//
+//		i = 0;
+//
+//		for (int row = 0; row < (radius * 2 + 1); row++) 
+//		{
+//			for (int col = 0; col < ((radius * 2 + 1) - Mathf.Abs (radius - row)); col++) 
+//			{
+//				float x = (col + Mathf.Abs (radius - row) / 2f - radius) * edgesize;
+//				float z = (radius - row) * triheight;
+//				float s = Mathf.Sqrt (x * x + z * z);
+//				float y = amplitude * Mathf.Cos (kmultiplier * s - (Time.fixedTime));
+//				vertices [i] = new Vector3 (x, y, z);
+//				i++;
+//
+//			}
+//		}
+//
+//		mesh.vertices = vertices;
+//		mesh.RecalculateNormals();
+	//	GetComponent<MeshCollider> ().sharedMesh = null;
+	//	GetComponent<MeshCollider> ().sharedMesh = mesh;
 	}
 }
 
