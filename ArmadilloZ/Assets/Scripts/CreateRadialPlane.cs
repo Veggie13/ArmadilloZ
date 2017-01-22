feac5cb8dd8134a179ef7e6084ab7c49a5fd7425
Veggie13 <cmderochie+github@gmail.com>
2017-01-21 17:57:15 -0700

    Added scene to main project and tied in wave physics.

--- ArmadilloZ/Assets/Scripts/CreateRadialPlane.cs
+++ ArmadilloZ/Assets/Scripts/CreateRadialPlane.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class CreateRadialPlane : MonoBehaviour {
 
 	MeshFilter mf;
-    const int radius = 10;
+    const int radius = 50;
     const float rad60 = 1.047197f;
-    const float edgesize = 0.25f;
+    const float edgesize = 0.5f;
     float triheight = edgesize * Mathf.Sin(rad60);
     int numvert = 0;
+    Vector2[] planePoints;
 
     // Use this for initialization
     void Start () {
@@ -25,7 +26,8 @@ public class CreateRadialPlane : MonoBehaviour {
 		numvert = numvert * 6 + 1;
 		int numtrianglepoints = radius * radius * 6 * 3; //Multiply by 3 since each triangle consists of 3 vertices
 
-		Vector3[] vertices = new Vector3[numvert];
+		var vertices = new Vector3[numvert];
+        planePoints = new Vector2[numvert];
 		int[] triangles = new int[numtrianglepoints];
 
 		i = 0;
@@ -36,6 +38,7 @@ public class CreateRadialPlane : MonoBehaviour {
 			{
 				float x = (col + Mathf.Abs (radius - row) / 2f - radius) * edgesize;
 				float z = (radius - row) * triheight;
+                planePoints[i] = new Vector2(x, z);
 				vertices [i] = new Vector3 (x, WaveField.GetAmplitude(x, z), z);
 				i++;
 
@@ -89,25 +92,79 @@ public class CreateRadialPlane : MonoBehaviour {
 		mesh.vertices = vertices;
 		mesh.triangles = triangles;
 		mesh.RecalculateNormals();
-		transform.gameObject.AddComponent<MeshCollider>();
+		//transform.gameObject.AddComponent<MeshCollider>();
 		transform.GetComponent<MeshCollider>().sharedMesh = mesh;
 
-	}
+        WaveField.forceMgr.Objects.Add(new WaveField.Movable()
+        {
+            Body = GameObject.Find("Sphere").GetComponent<Rigidbody>(),
+            DragCoefficient = 0.7f,
+            StopSpeed = 1f
+        });
+    }
 
 	void Update () {
 
-		Mesh mesh = mf.mesh;
-		for (int i = 0; i < mesh.vertices.Length; i++) 
+        Mesh mesh = mf.mesh;
+        var vertices = new Vector3[mesh.vertexCount];
+        for (int i = 0; i < vertices.Length; i++) 
 		{
-			float x = mesh.vertices[i].x;
-			float z = mesh.vertices[i].z;
-			mesh.vertices [i] = new Vector3 (x, WaveField.GetAmplitude(x, z), z);
+			float x = planePoints[i].x;
+			float z = planePoints[i].y;
+			vertices [i] = new Vector3 (x, WaveField.GetAmplitude(x, z), z);
 		}
 
-		//mesh.vertices = vertices;
+        mesh.vertices = vertices;
 		mesh.RecalculateNormals();
-		GetComponent<MeshCollider> ().sharedMesh = null;
-		GetComponent<MeshCollider> ().sharedMesh = mesh;
-	}
+        //GetComponent<MeshCollider> ().sharedMesh = null;
+        //GetComponent<MeshCollider> ().sharedMesh = mesh;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            var clickPos = Input.mousePosition;
+            var ray = Camera.main.ScreenPointToRay(clickPos);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                WaveField.forceMgr.Regions.Add(new WaveField.RadialWaveForceRegion()
+                {
+                    AnnularRegion = new WaveField.Annulus2()
+                    {
+                        Center = new Vector2(hit.point.x, hit.point.z),
+                        InnerRadius = 2f,
+                        OuterRadius = 4f,
+                        MinArc = Mathf.PI,
+                        MaxArc = Mathf.PI * (2f - 0.0f)
+                    },
+                    Center = new Vector2(hit.point.x, hit.point.z),
+                    Speed = 5f,
+                    UnitAmplitude = 2.4f,
+                    UnitWaveAmplitude = 1f,
+                    Wavelength = 0.5f
+                });
+                //WaveField.forceMgr.Regions.Add(new WaveField.RadialWaveForceRegion()
+                //{
+                //    AnnularRegion = new WaveField.Annulus2()
+                //    {
+                //        Center = new Vector2(hit.point.x, hit.point.z),
+                //        InnerRadius = 2f,
+                //        OuterRadius = 4f,
+                //        MinArc = Mathf.PI * (2f - 0.3f),
+                //        MaxArc = Mathf.PI * (2f - 0.2f)
+                //    },
+                //    Center = new Vector2(hit.point.x, hit.point.z),
+                //    Speed = 5f,
+                //    UnitAmplitude = 2.4f,
+                //    UnitWaveAmplitude = 1f,
+                //    Wavelength = 0.5f
+                //});
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        WaveField.forceMgr.Tick(Time.fixedDeltaTime);
+    }
 }
 
--- ArmadilloZ/Assets/Scripts/WaveField.cs
+++ ArmadilloZ/Assets/Scripts/WaveField.cs
@@ -1,11 +1,235 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public static class WaveField {
+    const float amplitude = 0.1f;
+    const float kmultiplier = 10f;
+
+    public abstract class Region2
+    {
+        public abstract bool Contains(Vector2 point);
+    }
+
+    //class Polygon2 : Region2
+    //{
+    //    public List<Vector2> Points = new List<Vector2>();
+
+    //    public override bool Contains(Vector2 point)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+
+    //class Circle2 : Region2
+    //{
+    //    public Vector2 Center;
+    //    public float Radius;
+
+    //    public override bool Contains(Vector2 point)
+    //    {
+    //        return (point - Center).Magnitude < Radius;
+    //    }
+    //}
+
+    public class Annulus2 : Region2
+    {
+        public Vector2 Center;
+        public float InnerRadius;
+        public float OuterRadius;
+        public float MinArc;
+        public float MaxArc;
+
+        public override bool Contains(Vector2 point)
+        {
+            var radial = point - Center;
+            float radius = radial.magnitude;
+            if (!(InnerRadius < radius && radius < OuterRadius)) return false;
+            float bearing = Mathf.Atan2(radial.y, radial.x);
+            double normBearing = (bearing - MinArc + Mathf.PI * 2) % (Mathf.PI * 2);
+            double arc = (MaxArc - MinArc + Mathf.PI * 2) % (Mathf.PI * 2);
+            return (arc == 0f) || (normBearing < arc);
+        }
+    }
+
+    public abstract class WaveForceRegion
+    {
+        public float Wavelength;
+
+        public abstract Region2 Region { get; }
+
+        public abstract float GetAmplitude(Vector2 pos);
+
+        public Vector2 GetVelocity(Vector2 pos)
+        {
+            if (!Region.Contains(pos))
+            {
+                return new Vector2();
+            }
+
+            return getVelocity(pos);
+        }
+
+        public Vector2 GetForce(Vector2 pos, Vector2 velocity)
+        {
+            if (!Region.Contains(pos))
+            {
+                return new Vector2();
+            }
+
+            var waveVelocity = getVelocity(pos);
+            float relativeSpeed = Vector2.Dot(velocity - waveVelocity, waveVelocity) * -1f;
+            if (relativeSpeed < 0f)
+            {
+                return new Vector2();
+            }
+
+            return waveVelocity.normalized * relativeSpeed * GetAmplitude(pos);
+        }
+
+        protected abstract Vector2 getVelocity(Vector2 pos);
+
+        public abstract float GetWaveValue(Vector2 pos);
+
+        public abstract bool Tick(float dt);
+    }
+
+    //class LinearWaveForceRegion : WaveForceRegion
+    //{
+    //    public float Amplitude;
+    //    public Vector2 Velocity;
+
+    //    public override float GetAmplitude(Vector2 pos)
+    //    {
+    //        return Amplitude;
+    //    }
+
+    //    protected override Vector2 getVelocity(Vector2 pos)
+    //    {
+    //        return Velocity;
+    //    }
+    //}
+
+    public class RadialWaveForceRegion : WaveForceRegion
+    {
+        public Annulus2 AnnularRegion;
+        public Vector2 Center;
+        public float Speed;
+        public float UnitAmplitude;
+        public float UnitWaveAmplitude;
+
+        public override Region2 Region
+        {
+            get
+            {
+                return AnnularRegion;
+            }
+        }
+
+        protected override Vector2 getVelocity(Vector2 pos)
+        {
+            var radial = pos - Center;
+            return radial.normalized * Speed;
+        }
+
+        public override float GetAmplitude(Vector2 pos)
+        {
+            if (!Region.Contains(pos))
+            {
+                return 0f;
+            }
+            var radial = pos - Center;
+            float interp = (radial.magnitude - AnnularRegion.InnerRadius) / (AnnularRegion.OuterRadius - AnnularRegion.InnerRadius);
+            return UnitAmplitude * interp;
+        }
+
+        public override bool Tick(float dt)
+        {
+            AnnularRegion.OuterRadius += Speed * dt;
+            AnnularRegion.InnerRadius += Speed * dt;
+            return (AnnularRegion.OuterRadius < 30f);
+        }
+
+        public override float GetWaveValue(Vector2 pos)
+        {
+            if (!Region.Contains(pos))
+            {
+                return 0f;
+            }
+            var radial = pos - Center;
+            float interp = (radial.magnitude - AnnularRegion.InnerRadius) / (AnnularRegion.OuterRadius - AnnularRegion.InnerRadius);
+            float phase = (1f - interp) / Wavelength;
+            return UnitWaveAmplitude * interp * Mathf.Sin(phase * Mathf.PI * 2);
+        }
+    }
+
+    public class Movable
+    {
+        public Rigidbody Body;
+        public Vector2 Position
+        {
+            get
+            {
+                return new Vector2(Body.position.x, Body.position.z);
+            }
+        }
+        public Vector2 Velocity
+        {
+            get
+            {
+                return new Vector2(Body.velocity.x, Body.velocity.z);
+            }
+        }
+        public float DragCoefficient;
+        public float StopSpeed;
+
+        public void AcceptForce(Vector2 force, float dt)
+        {
+            Body.AddForce(new Vector3(force.x, 0f, force.y), ForceMode.Acceleration);
+        }
+
+        public void Tick(float dt)
+        {
+            var planeVelocity = new Vector2(Body.velocity.x, Body.velocity.z);
+            planeVelocity *= Mathf.Pow(DragCoefficient, dt);
+            if (planeVelocity.magnitude < StopSpeed)
+            {
+                planeVelocity = Vector2.zero;
+            }
+            Body.velocity = new Vector3(planeVelocity.x, Body.velocity.y, planeVelocity.y);
+        }
+    }
+
+    public class ForceManager
+    {
+        public List<Movable> Objects = new List<Movable>();
+        public List<WaveForceRegion> Regions = new List<WaveForceRegion>();
+
+        public void Tick(float dt)
+        {
+            foreach (var region in Regions.ToArray())
+            {
+                if (!region.Tick(dt)) Regions.Remove(region);
+            }
+            foreach (var obj in Objects)
+            {
+                foreach (var region in Regions)
+                {
+                    obj.AcceptForce(region.GetForce(obj.Position, obj.Velocity), dt);
+                }
+
+                obj.Tick(dt);
+            }
+        }
+    }
+
+    public static ForceManager forceMgr = new ForceManager();
+
     public static float GetAmplitude(float x, float z)
     {
-        float s = Mathf.Sqrt(x * x + z * z);
-        float y = amplitude * Mathf.Cos(kmultiplier * s - (Time.fixedTime));
+        var pos = new Vector2(x, z);
+        float y = forceMgr.Regions.Sum(r => r.GetWaveValue(pos));
+        return y;
     }
 }
