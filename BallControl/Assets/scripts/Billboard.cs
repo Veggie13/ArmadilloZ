3c8c94228aa58bc9ac6bde4cae3ac5da94e701dd
NolanHanna <nolan_rh@hotmail.com>
2017-01-21 22:08:21 -0700

    Added in some sprite sheets and animations as well as a script to control them.

---
+++ BallControl/Assets/scripts/Billboard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private void LateUpdate()
+    {
+        if (Camera.main != null)
+        {
+            transform.LookAt(Camera.main.transform.position, Camera.main.transform.up);
+            //transform.Rotate(Vector3.up, 180);
+        }
+    }
+}
